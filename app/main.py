2fd987d959ee0ebfab78a740ddaaa34ff64f09a5
juanE <55924277+je-vargas@users.noreply.github.com>
2022-02-15 18:48:44 +0000

    DB connection complete and working

--- app/main.py
+++ app/main.py
@@ -8,6 +8,8 @@ import psycopg2
 import time
 
 class UserLogin(BaseModel):
+    firstname: str
+    lastname: str
     username: str
     password: str
     created: Optional[datetime] = None
@@ -50,6 +52,7 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"Health": "OK"}
+
 # #* q here is a query string passed in the url http://localhost:8000/items/5?q=somequery
 # @app.get("/items/{item_id}")
 # def read_item(item_id: int, q: Optional[str] = None):
@@ -57,30 +60,51 @@ def read_root():
 
 @app.post("/users/login", status_code=status.HTTP_201_CREATED)
 async def user_login(new_user : UserLogin):
+
+    post = None
+
     try:
         valid_email = validate_email(new_user.username)
     except EmailNotValidError as error: 
         raise HTTPException(status.HTTP_400_BAD_REQUEST,f"{error}")
 
+    # try: 
+    #     # check user doens't exist otherwise add them
+    #     cursor.execute(f''' SELECT * FROM test WHERE id = '{new_user.username}' ''')
+    #     post = cursor.fetchone()
+    # except Exception as error: 
+    #     print(f"\nError: {error}")
+
     try: 
-        # check user doens't exist otherwise add them
-        cursor.execute(f''' SELECT * FROM test WHERE id = {new_user.username} ''')
-        post = cursor.fetchone()
+        # add user to system
+        sql = "INSERT INTO test (firstname, lastname, username, password) VALUES (%s, %s, %s, %s) ON CONFLICT (username) DO NOTHING " 
+        data = new_user.firstname, new_user.lastname, new_user.username, new_user.password
+        cursor.execute(sql, data)
+        connection.commit()
+
     except Exception as error: 
         print(f"\nError: {error}")
 
-
-    if post == None or post == '':
-        try: 
-        # add user to system
-            cursor.execute(f''' SELECT * FROM test WHERE id = {new_user.username} ''')
-            post = cursor.fetchone()
-        except Exception as error: 
-            print(f"\nError: {error}")
-
-
-
-
-
     return {"Successul": new_user}
     
+
+@app.get("/users/{user_id}", status_code=status.HTTP_200_OK)
+def get_user_by_id(user_id: int):
+
+    cursor.execute(''' SELECT * FROM test WHERE id = (%s)''', str(user_id))
+    user = cursor.fetchone()
+
+    if not user: raise HTTPException(status.HTTP_404_NOT_FOUND, f"User with ID {user_id} was not found")
+
+    return {"User": user}
+
+@app.delete("/users/delete/{user_id}", status_code=status.HTTP_200_OK)
+def delete_user_by_id(user_id: int):
+
+    cursor.execute(''' DELETE FROM test WHERE id = (%s) RETURNING * ''', str(user_id))
+    deleted_user = cursor.fetchone()
+    connection.commit()
+
+    if not deleted_user == None: raise HTTPException(status.HTTP_404_NOT_FOUND, f"User with ID {user_id} was not found")
+
+    return Response(status.HTTP_204_NO_CONTENT)
