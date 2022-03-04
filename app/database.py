315ed11fd5c44060f73573ce7ecbb81ee7f32d23
juanE <55924277+je-vargas@users.noreply.github.com>
2022-03-04 11:24:40 +0000

    database tweaks to use environment variables

--- app/database.py
+++ app/database.py
@@ -1,8 +1,12 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from .config import get_settings
 
-SQLALCHEMY_DATABASE_URL = "postgresql://juanestebanvargassalamanca:@localhost/elvolunteer"
+
+settings = get_settings()
+# SQLALCHEMY_DATABASE_URL = "postgresql://juanestebanvargassalamanca:@localhost/elvolunteer"
+SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.database_username}:@{settings.database_hostname}/{settings.database_name}"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
