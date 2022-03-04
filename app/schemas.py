5a5e1845f903877bddcf5fd1f0ede5bf57bb8a45
juanE <55924277+je-vargas@users.noreply.github.com>
2022-03-04 11:24:15 +0000

    tweaks to models, shcemas

--- app/schemas.py
+++ app/schemas.py
@@ -3,14 +3,9 @@ from pydantic import BaseModel, EmailStr
 from typing import Optional
 from datetime import datetime
 
-class UserIn(BaseModel):
-    firstname: str
-    lastname: str
-    username: str
+class Credentials(BaseModel):
+    username: EmailStr
     password: str
-    createdOn: Optional[datetime] = None
-    class Config:
-        orm_mode = True
 
     @validator("username")
     def validate_login_username(cls, username):
@@ -25,6 +20,19 @@ class UserIn(BaseModel):
         if len(password) < 8 : raise ValueError("Password must be 8 characters long")
         return password
 
+class UserCredentials(Credentials):
+    date: Optional[datetime] = None
+
+class UserLogInResponse(BaseModel):
+    token: str
+
+class UserIn(Credentials):
+    firstname: str
+    lastname: str
+    createdOn: Optional[datetime] = None
+    class Config:
+        orm_mode = True
+
 class UserOut(BaseModel):
     id = int
     username = EmailStr
@@ -33,6 +41,34 @@ class UserOut(BaseModel):
         orm_mode = True
 
 class Email(BaseModel):
-    username: str
+    username: EmailStr
     class Config:
         orm_mode = True
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+class TokenData(BaseModel):
+    id: Optional[str] = None
+
+class EmployerOut(BaseModel):
+    id: int
+    companyName: str
+    companyDescription: str 
+
+class Job(BaseModel):
+    id = int
+    jobDescription = str
+    jobTitle = str
+    numberOfPositions = int
+    onGoingFill = bool
+    startDate = datetime
+    endDate = datetime
+    applicationDeadline = datetime
+class JobIn(BaseModel):
+    employer_id = int
+    category_id = int
+class JobOut(Job):
+    employer = EmployerOut
+    
