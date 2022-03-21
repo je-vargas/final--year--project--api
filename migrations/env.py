8fe6e43ab41a6b355428aa1b6cd4c10c1df7cd5f
juanE <55924277+je-vargas@users.noreply.github.com>
2022-03-21 10:42:23 +0000

    DSP-8: setting up environment variables , and removing database build from main

--- migrations/env.py
+++ migrations/env.py
@@ -1,13 +1,17 @@
 from logging.config import fileConfig
-
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
 from alembic import context
 
+from app.config import get_settings
+from app.models import Base
+
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
+settings = get_settings()
 config = context.config
+config.set_main_option("sqlalchemy.url", f"postgresql://{settings.database_username}:@{settings.database_hostname}/{settings.database_name}")
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
@@ -17,7 +21,7 @@ fileConfig(config.config_file_name)
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
