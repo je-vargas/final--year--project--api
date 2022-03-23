25e3e99ef0a2a4e1342b4194a55f115457dc5add
juanE <55924277+je-vargas@users.noreply.github.com>
2022-03-23 11:25:11 +0000

    DSP-6: separet some of the user schemas into controller specific folder

--- app/Schemas/users.py
+++
@@ -1,9 +0,0 @@
-from typing import Optional
-from datetime import datetime
-from pydantic import (
-    BaseModel, 
-    ValidationError, 
-    validator, 
-    EmailStr
-    )
-
---
+++ app/Schemas/usersSchemas.py
@@ -0,0 +1,42 @@
+from typing import Optional
+from datetime import datetime
+from pydantic import (
+    BaseModel, 
+    ValidationError, 
+    validator, 
+    EmailStr, 
+    Field
+    )
+
+class NewAccountSchemaIn(BaseModel):
+    username: EmailStr
+    password: str
+    firstName: str
+    lastName: str
+    telephoneNumber: str
+    dateCreated: datetime
+    lastLogin: datetime
+
+    @validator("username")
+    def validate_login_username(cls, username):
+        if username == "": 
+            raise ValueError("Username must not be empty")
+        return username
+    
+    @validator("password")
+    def validate_login_password(cls, password):
+        if password == "": raise ValueError("Password must not be empty")
+        if len(password) < 8 : raise ValueError("Password must be 8 characters long")
+        return password
+
+class NewAccountSchemaOut(BaseModel):
+    id: int
+    username: EmailStr
+    firstName: str
+    lastName: str
+    telephoneNumber: str
+    accountRoleId: int
+    dateCreated: datetime
+  
+
+
