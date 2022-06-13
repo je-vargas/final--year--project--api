474f97357043302a7b03b235299fea86b845551d
juanE <55924277+je-vargas@users.noreply.github.com>
2022-06-13 12:49:04 +0100

    DSP-10: update and creation validation added on name

--- app/Schemas/usersSchemas.py
+++ app/Schemas/usersSchemas.py
@@ -33,6 +33,18 @@ class NewAccountCompanySchemaIn(BaseModel):
         if len(password) < 8 : raise ValueError("Password must be 8 characters long")
         return password
     
+    @validator("firstName")
+    def validate_login_firstName(cls, firstName):
+        if firstName == "": 
+            raise ValueError("First name must not be empty")
+        return username
+    
+    @validator("lastName")
+    def validate_login_lastName(cls, lastName):
+        if lastName == "": 
+            raise ValueError("Last name must not be empty")
+        return username
+
     @validator("telephoneNumber")
     def validate_telphone(cls, telephoneNumber):
         telPattern = '^(0|\+44)(\d{10}|\d{2}\s\d{4}\s\d{4}|\s\d{4}\s\d{6})$'
@@ -73,6 +85,18 @@ class NewAccountSchemaIn(BaseModel):
             raise ValueError("Username must not be empty")
         return username
     
+    @validator("firstName")
+    def validate_login_firstName(cls, firstName):
+        if firstName == "": 
+            raise ValueError("First name must not be empty")
+        return username
+    
+    @validator("lastName")
+    def validate_login_lastName(cls, lastName):
+        if lastName == "": 
+            raise ValueError("Last name must not be empty")
+        return username
+    
     @validator("password")
     def validate_login_password(cls, password):
         if password == "": raise ValueError("Password must not be empty")
@@ -95,7 +119,6 @@ class NewAccountSchemaOut(BaseModel):
     accountRoleId: int
     dateCreated: datetime
 
-
 class NewAccountCompanySchemaOut(BaseModel):
     id: int
     username: EmailStr
@@ -112,12 +135,53 @@ class UpdateAccountSchema(BaseModel):
     firstName: Optional[str] = None
     lastName: Optional[str] = None
     telephoneNumber: Optional[str] = None
+    companyName: Optional[str] = None
+    companyDescription: Optional[str] = None 
+    industry: Optional[IdustryEnum] = None
 
     @validator("username")
     def validate_login_username(cls, username):
         if username == "": 
             raise ValueError("Username must not be empty")
         return username
+    
+    @validator("firstName")
+    def validate_login_firstName(cls, firstName):
+        if firstName == "": 
+            raise ValueError("First name must not be empty")
+        return username
+    
+    @validator("lastName")
+    def validate_login_lastName(cls, lastName):
+        if lastName == "": 
+            raise ValueError("Last name must not be empty")
+        return username
+    
+    @validator("telephoneNumber")
+    def validate_telphone(cls, telephoneNumber):
+        telPattern = '^(0|\+44)(\d{10}|\d{2}\s\d{4}\s\d{4}|\s\d{4}\s\d{6})$'
+        validTel = re.match(telPattern, telephoneNumber)
+        if not validTel : raise ValueError("Invalid telephone number | Enter an 11 digit number beggining with 0 or +44")
+        return telephoneNumber
+
+    @validator("companyName")
+    def validate_company(cls, companyName):
+        if companyName == "": 
+            raise ValueError(" Company name must not be empty")
+        return 
+    
+    @validator("companyDescription")
+    def validate_company_description(cls, companyDescription):
+        if companyDescription == "": 
+            raise ValueError(" Company Description must not be empty")
+        return 
+    
+    @validator("industry")
+    def validate_industry(cls, industry):
+        if industry == "": 
+            raise ValueError(" Industry must not be empty")
+        return 
+
 
 
 
