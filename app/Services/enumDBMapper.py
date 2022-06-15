d90e331b08a95fce5a4df90d1bb07561f01b29fc
juanE <55924277+je-vargas@users.noreply.github.com>
2022-06-15 16:38:15 +0100

    DSP-10: mappers for enum values added

--- app/Services/enumDBMapper.py
+++ app/Services/enumDBMapper.py
@@ -1,4 +1,4 @@
-from ..globalEnums import IndustryDBMapper
+from ..globalEnums import IndustryDBMapper, CategoryDBMapper
 
 def industry_name_to_id_db_mapper(idustry_name):
     for industry in IndustryDBMapper:
@@ -9,3 +9,13 @@ def industry_id_to_name_db_mapper(idustry_id):
     for industry in IndustryDBMapper:
             if industry.value == idustry_id: 
                 return industry.name 
+
+def category_name_to_id_db_mapper(category_name):
+    for category in CategoryDBMapper:
+            if category.name.lower() == category_name.lower(): 
+                return category.value 
+
+def category_id_to_name_db_mapper(category_id):
+    for category in CategoryDBMapper:
+            if category.value == category_id: 
+                return category.name 
