5c6cf5968a5dfe92f5e618466b09197b16c9317b
juanE <55924277+je-vargas@users.noreply.github.com>
2022-06-16 13:53:42 +0100

    DSP-10: enpoint for deleting jobs complete

--- app/Repositories/jobRepository.py
+++ app/Repositories/jobRepository.py
@@ -10,3 +10,31 @@ def create_new_job(new_job: Jobs, db):
         raise HTTPException(status.HTTP_400_BAD_REQUEST, new_job, e.orig.pgerror)
         
     return new_job
+
+def get_job_by_job_and_employer_id(job_id: int, employer_id: int, db):
+    job = None
+    try:
+        job = db.query(CompanyRepresentative, Jobs).\
+            join(Jobs, CompanyRepresentative.id == Jobs.companyRepresentative_id).\
+            filter(CompanyRepresentative.userAccount_id == employer_id).\
+            filter(Jobs.id == job_id)
+    except Exception as e:
+        raise HTTPException(status.HTTP_400_BAD_REQUEST, "\nCouln't fetch job for user with id: {0} | Error: {1}\n".format(employer_id, e.orig.pgerror))
+    return job
+
+def get_job_by_id(job_id: int, db):
+    job = None
+    try:
+        job = db.query(Jobs).filter(Jobs.id == job_id)
+
+    except Exception as e:
+        raise HTTPException(status.HTTP_400_BAD_REQUEST, "\nCouln't fetch job for user with id: {0} | Error: {1}\n".format(employer_id, e.orig.pgerror))
+    return job
+
+
+def delete_job_by_object(job_object, db):
+    try:
+        job_object.delete(synchronize_session=False)
+        db.commit()
+    except Exception as e:
+        raise HTTPException(status.HTTP_400_BAD_REQUEST, e.orig.pgerror)
