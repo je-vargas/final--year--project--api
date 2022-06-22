1ac93770a95cefab6800a3eb0069a4fb78b49f03
juanE <55924277+je-vargas@users.noreply.github.com>
2022-06-22 17:00:36 +0100

    DSP-10: method added to update job

--- app/Repositories/jobRepository.py
+++ app/Repositories/jobRepository.py
@@ -11,7 +11,7 @@ def create_new_job(new_job: Jobs, db):
         
     return new_job
 
-def get_job_by_job_and_employer_id(job_id: int, employer_id: int, db):
+def get_job_by_id_and_employer_id(job_id: int, employer_id: int, db):
     job = None
     try:
         job = db.query(CompanyRepresentative, Jobs).\
@@ -30,6 +30,19 @@ def get_job_by_id(job_id: int, db):
     except Exception as e:
         raise HTTPException(status.HTTP_400_BAD_REQUEST, "\nCouln't fetch job for user with id: {0} | Error: {1}\n".format(employer_id, e.orig.pgerror))
     return job
+
+
+def update_job_by_id(id, job_update, db):
+    try:
+        query = db.query(Jobs).filter(Jobs.id == id)
+        query.update(job_update, synchronize_session=False)
+        db.commit()
+        updated_job = query.first()
+        
+    except Exception as e:
+        raise HTTPException(status.HTTP_400_BAD_REQUEST, e.orig.pgerror)
+
+    return updated_job
 
 
 def delete_job_by_object(job_object, db):
